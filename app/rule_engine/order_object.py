da75c33f1e3d27ae201829b26953d22ae5e07b7f
AidanMaven <aidan@mavenmedical.net>
2014-02-10 17:26:50 -0500

    MAV-40 adding type checks to order object

--- app/rule_engine/order_object.py
+++ app/rule_engine/order_object.py
@@ -21,6 +21,16 @@ class OrderObject():
 
     def __init__(self, order_id, order_parameters):
         self.order_id = order_id
+        if (type(order_parameters[0]) != str):
+            raise TypeError("The order type parameter must be a string")
+        if (type(order_parameters[1]) != int):
+            raise TypeError("The order CPT parameter must be an integer")
+        if (type(order_parameters[2]) != int):
+            raise TypeError("The age parameter must be an integer")
+        if (type(order_parameters[3]) != list or type(order_parameters[3][0]) != int):
+                raise TypeError("The snomed id parameter must be a list of integers")
+        if (type(order_parameters[4]) != str):
+            raise TypeError("The order department parameter must be a string")
         self.d = {
             order_type: order_parameters[0],
             CPT: order_parameters[1],
