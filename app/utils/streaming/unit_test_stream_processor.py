c9f61bf698297f05c349af3b7f868d90cd5ec821
Tom DuBois <tom@mavenmedical.net>
2014-02-10 11:09:21 -0500

    fixing up some pycharm code style warnings

--- app/utils/streaming/unit_test_stream_processor.py
+++ app/utils/streaming/unit_test_stream_processor.py
@@ -20,39 +20,40 @@ import app.utils.streaming.stream_processor as SP
 
 teststreamname = 'test stream'
 MavenConfig = {
-    teststreamname: {
-        'readertype':'Testing',
-        'readerconfig':'Testing',
-        'writertype':'Testing',
-        'writerconfig':'Testing'
-        }
+    teststreamname:
+    {
+        'readertype': 'Testing',
+        'readerconfig': 'Testing',
+        'writertype': 'Testing',
+        'writerconfig': 'Testing'
     }
+}
 
-MC.MavenConfig=MavenConfig
+MC.MavenConfig = MavenConfig
 
-SP.MavenConfig=MavenConfig
 
 class ConcatenateStreamProcessor(SP.StreamProcessor):
     
-    def __init__(self,configname):
-        SP.StreamProcessor.__init__(self,configname)
-        self.master_list=['','','','','']
+    def __init__(self, configname):
+        SP.StreamProcessor.__init__(self, configname)
+        self.master_list = ['', '', '', '', '']
         
-    def read_object(self,obj):
+    def read_object(self, obj):
         self.master_list.append(obj)
         self.master_list.pop(0)
         self.write_object(self.master_list)
 
 sp = ConcatenateStreamProcessor(teststreamname)
 
-if( __name__ == '__main__' ):
+if __name__ == '__main__':
     def unpickle_writer(obj):
         print(pickle.loads(obj))
 else:
-    output_buffer=''
+    output_buffer = ''
+
     def unpickle_writer(obj):
         global output_buffer
-        output_buffer+=str(pickle.loads(obj))+"\n"
+        output_buffer += str(pickle.loads(obj))+"\n"
         
 sp.write_raw = unpickle_writer
 
@@ -62,6 +63,6 @@ for x in range(10):
     sp.read_object(x)
 
 try:
-    result=output_buffer.strip()
+    result = output_buffer.strip()
 except NameError:
     pass
