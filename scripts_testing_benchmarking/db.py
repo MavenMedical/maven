9cec06f056ea5742efb3d57e7d3983306b8d6331
Tom DuBois <tom@mavenmedical.net>
2014-02-24 09:06:02 -0500

    got it working, not cleaned up yet

--- scripts_testing_benchmarking/db.py
+++ scripts_testing_benchmarking/db.py
@@ -1,5 +1,7 @@
 import psycopg2, asyncio
 from app.database_utils.dbconfig import CONNECTION_STRING
+import sys
+import traceback
 
 import maven_logging as ML
 ML.DEBUG=ML.stdout_log_with_time
@@ -7,19 +9,116 @@ ML.DEBUG=ML.stdout_log_with_time
 con_id = 0
 cur_id = 0
 
+
+class SocketReadyFuture(asyncio.Future):
+    final read = True, write = False
+
+    def __init__(self,is_reader, fileno, loop):
+        asyncio.Future.__init__(self)
+        self.is_reader = is_reader
+        self.loop = loop
+        self.fileno = fileno
+        if(is_reader):
+            loop.add_reader(fileno, wake_future, self)
+        else:
+            loop.add_writer(fileno, wake_future, self)
+
+    def wake_future(self):
+        if(self.is_reader):
+            self.loop.remove_reader(self.fileno)
+        else:
+            self.loop.remove_writer(self.fileno)
+        self.set_result(None)
+
+
+#def wake_future(fut):
+#    print("in wake_future() %d" % fut.fileno)
+#    try:
+#        fut.loop.remove_reader(fut.fileno)
+#    except Exception:
+#        pass
+#    try:
+#        fut.loop.remove_writer(fut.fileno)
+#    except Exception:
+#        pass
+#    sys.stdout.flush()
+#    #if(not fut.done()):
+#    fut.set_result(None)
+#    print("set the future's results")
+#2A    print(fut)
+
+
 class Connection():
 
     def __init__(self):
         global con_id
+        print("in constructor")
         con_id += 1
+        self.cursors = {}
+
+    @asyncio.coroutine
+    def connect(self, loop):
+        self.loop=loop
         ML.DEBUG("about to connect #%d" % con_id)
+        #sys.stdout.flush()
         self.connection = psycopg2.connect(CONNECTION_STRING, async=1)
         ML.DEBUG("connected #%d" % con_id)
-        self.connection.set_session(autocommit=True)
-        self.cursors = {}
+        yield from self.wait()
+
+
+    @asyncio.coroutine
+    def wait(self):
+        #print("in self.wait()")
+        try:
+            while True:
+                state = self.connection.poll()
+                print("state is %d" % state)
+                sys.stdout.flush()
+                if state == psycopg2.extensions.POLL_OK:
+                #print("breaking write away")
+                    break
+                elif state == psycopg2.extensions.POLL_WRITE:
+                #print("process write")
+                    fut = asyncio.Future()
+                    fut.loop = self.loop
+                    fut.fileno =self.connection.fileno()
+                    print("created future")
+                    self.loop.add_writer(self.connection.fileno(),wake_future,fut)
+                    #sem=asyncio.Semaphore()
+                    #self.loop.add_writer(self.connection.fileno(),wake_sem,sem)
+                    print("yielding")
+                    sys.stdout.flush()
+                    if not fut.done():
+                        yield from fut
+                    #yield from asyncio.wait_for(fut)
+                    print("disappear")
+                    sys.stdout.flush()
+                #self.loop.remove_writer(self.connection.fileno())
+                elif state == psycopg2.extensions.POLL_READ:
+                    fut = asyncio.Future()
+                    fut.loop = self.loop
+                    fut.fileno =self.connection.fileno()
+                    self.loop.add_reader(self.connection.fileno(),wake_future,fut)
+                    print("yielding")
+                    sys.stdout.flush()
+                    if not fut.done():
+                        yield from fut
+                #self.loop.remove_reader(self.connection.fileno())
+                    print("disappear")
+                sys.stdout.flush()
+            else:
+                print("error")
+                raise psycopg2.OperationalError("poll() returned %s" % state)
+        except Exception:
+            print("ERROR HERE")
+            traceback.print_exc(file=sys.stdout)
+            sys.stdout.flush()
+        print("end of the run-on function")
+
 
     def get_cursor(self):
         global cur_id
+        print("get_cursor")
         cur_id += 1
         ML.DEBUG("creating cursor #%d" % cur_id)
         cur = self.connection.cursor()
@@ -27,12 +126,14 @@ class Connection():
         self.cursors[cur]=cur_id
         return cur
 
+    @asyncio.coroutine
     def execute(self, cur, str, extra=None):
         ML.DEBUG('about to execute %s on %d' % (str.split(' ', 1)[0], self.cursors[cur]))
         if extra:
             cur.execute(str,extra)
         else:
             cur.execute(str)
+        yield from self.wait()
         ML.DEBUG('executed on %d' % self.cursors[cur])
 
     def iterate_and_print(self,cur):
@@ -44,14 +145,31 @@ class Connection():
         ML.DEBUG("done iterating %d" % self.cursors[cur])
 
 
-def db_main():
+@asyncio.coroutine
+def db_main(loop):
+    print("conn - Connection()")
     conn = Connection()
+    #print(1.5)
+    print(conn)
+    sys.stdout.flush()
+    cc=conn.connect(loop)
+    print(cc)
+    sys.stdout.flush()
+
+    yield from cc
+    print(2)
+    print(str(conn))
+    print(3)
+    sys.stdout.flush()
     cur = conn.get_cursor()
+    sys.stdout.flush()
+    print(cur)
     try: 
-        conn.execute(cur,'drop table TomTest')
+        print(3)
+        yield from conn.execute(cur,'drop table TomTest')
     except psycopg2.ProgrammingError:
         pass
-    conn.execute(cur,'create table TomTest (id serial PRIMARY KEY, num integer, data varchar);')
+    yield from conn.execute(cur,'create table TomTest (id serial PRIMARY KEY, num integer, data varchar);')
     
     rows=[]
     #for i in range(10):
@@ -63,8 +181,8 @@ def db_main():
     for i in range(100000):
         rows.append((i, 'string %d' % i))
     args_str = b','.join(cur.mogrify("%s", (x, )) for x in rows)
-    conn.execute(cur,"INSERT INTO tomtest (num, data) VALUES "+args_str.decode("utf-8"))
-    conn.execute(cur,'select * from TomTest')
+    yield from conn.execute(cur,"INSERT INTO tomtest (num, data) VALUES "+args_str.decode("utf-8"))
+    yield from conn.execute(cur,'select * from TomTest')
     conn.iterate_and_print(cur)
     cur.close()
     print("current is closed")
@@ -72,8 +190,8 @@ def db_main():
 @asyncio.coroutine
 def process(loop, f, id):
     for x in range(2):
-        #print("hello from %s" % id)
-        db_main()
+        print("hello from %s" % id)
+        yield from db_main(loop)
         print("%s sleeping" % id)
         yield from asyncio.sleep(.5)
         print("%s woke up" % id)
@@ -82,92 +200,10 @@ def process(loop, f, id):
         f.set_result(None)
 
 
-def wait_read_socket(fd):
-    loop = asyncio.get_event_loop()
-    fut = asyncio.Future(loop=loop)
-    loop.add_reader(fd, fut.set_result,None)
-    print("returning future %s" % str(fut))
-    return fut
-
-def wait_write_socket(fd):
-    loop = asyncio.get_event_loop()
-    print("1")
-    fut = asyncio.Future(loop=loop)
-    print("2")
-    loop.add_writer(fd, fut.set_result,None)
-    print("returning future %s" % str(fut))
-    return fut
-    
-
-def wait_select(conn):
-    """Wait until a connection or cursor has data available.                    
-                                                                                
-    The function is an example of a wait callback to be registered with         
-    `~psycopg2.extensions.set_wait_callback()`. This function uses              
-    :py:func:`~select.select()` to wait for data available.                     
-                                                                                
-    """
-    import select
-    from psycopg2.extensions import POLL_OK, POLL_READ, POLL_WRITE
-
-    print("in supplied wait_select")
-
-    while 1:
-        state = conn.poll()
-        if state == POLL_OK:
-            break
-        elif state == POLL_READ:
-            select.select([conn.fileno()], [], [])
-        elif state == POLL_WRITE:
-            select.select([], [conn.fileno()], [])
-        else:
-            raise conn.OperationalError("bad state from poll: %s" % state)
-
-def local_wait_select(conn):
-    """Wait until a connection or cursor has data available.                    
-                                                                                
-    The function is an example of a wait callback to be registered with         
-    `~psycopg2.extensions.set_wait_callback()`. This function uses              
-    :py:func:`~select.select()` to wait for data available.                     
-                                                                                
-    """
-    print("in wait select")
-    from psycopg2.extensions import POLL_OK, POLL_READ, POLL_WRITE
-
-    while 1:
-        state = conn.poll()
-        fileno = conn.fileno()
-        loop = asyncio.get_event_loop()
-        print(loop)
-        if state == POLL_OK:
-            print("file %d is OK" % fileno)
-            break
-        elif state == POLL_READ:
-            print("file %d trying to read" % fileno)
-            fut = wait_read_socket(fileno)
-            print("run until completed "+ str(fut))
-            loop.run_until_complete(fut)
-            print("done run until completed "+ str(fut))
-            #yield from fut
-            loop.remove_reader(fileno)
-        elif state == POLL_WRITE:
-            print("file %d trying to write" % fileno)
-            fut = wait_write_socket(fileno)
-            print("run until completed "+ str(fut))
-            loop.run_until_complete(fut)
-            print("done run until completed "+ str(fut))
-            #yield from fut
-            loop.remove_writer(fileno)
-        else:
-            raise conn.OperationalError("bad state from poll: %s" % state)
-
-
 def async_main():
     loop = asyncio.get_event_loop()
     print(loop)
     f = asyncio.Future()
-    #psycopg2.extensions.set_wait_callback(local_wait_select)
-    #psycopg2.extensions.set_wait_callback(wait_select)
     tasks = [asyncio.Task(x) for x in [process(loop,f,'p1'),process(loop,f,'p2')]]
     f.count = len(tasks)
     print("Starting loop")
