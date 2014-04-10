69dd4ab3814fe87541ddcd2bb72728fecae4f3de
Tom DuBois <tom@mavenmedical.net>
2014-04-10 16:27:13 -0400

    prototyped autocomplete web service and UI

---
+++ app/frontend_web/prototypes/autocomplete/autocomplete.js
@@ -0,0 +1,22 @@
+$(function() {
+	$("#searchb").autocomplete({
+		source: "/ac/autocomplete",
+		    crossDomain:true,
+		    minLength: 2,
+		    select: function(event, ui) {
+		    var url = ui.item.id;
+		    alert('url');
+		    if(url != '#') {
+			alert(url);
+		    }
+		},
+ 
+		    html: true, // optional (jquery.ui.autocomplete.html.js required)
+ 
+		    // optional (if other layers overlap autocomplete list)
+		    open: function(event, ui) {
+		    $(".ui-autocomplete").css("z-index", 1000);
+		}
+	    });
+ 
+    });
