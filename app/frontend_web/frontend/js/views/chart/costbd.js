6b50d555b2eed49621d0b83325075b0479c273d8
Tom DuBois <tom@mavenmedical.net>
2014-04-22 17:49:04 -0400

    making the daily spend chart and the pie chart talk to each other

--- app/frontend_web/frontend/js/views/chart/costbd.js
+++ app/frontend_web/frontend/js/views/chart/costbd.js
@@ -16,21 +16,71 @@ define([
         el: '.costbd',
         template: _.template(costbdTemplate),
         initialize: function(){
-            _.bindAll(this, 'render', 'click');
+		//_.bindAll(this, 'render', 'click');
             this.render();
-        },
-        events:{
-            'click': 'click'
-        },
-        click:function(){
-          console.log('clicked');
-        },
+	    },
         render: function () {
-            this.$el.html(this.template({page: currentContext.get('page')}));
-            return this;
-        }
-    });
+		this.$el.html(this.template({page: currentContext.get('page')}));
+		return this;
+	    },	
+	update: function(spend) {
+		var datefilter=null;
+		if('title' in spend.changed) {
+		    return;
+		}
+		if('date' in spend.changed) {
+		    datefilter=spend.changed['date'];
+		}
+		var colorArray = ["#0188BB", "#4C2694", "#79B32D", "#FF8500", "#00587A" ]
+		//Palette URL: http://colorschemedesigner.com/#3q62mWSE5w0w0
+
+		var gathered = {};
+		var data = [];
+
+		for (var d in spend.attributes) {
+		    if(Date.parse(d)) {
+			var date = new Date(d);
+			if(!datefilter || 
+			   (date.getFullYear()==datefilter.getFullYear() && 
+			    date.getMonth()==datefilter.getMonth() &&
+			    date.getDate()==datefilter.getDate())) {
+			    var days_spend = spend.get(d);
+			    for (var t in days_spend) {
+				var base = days_spend[t];
+				if (t in gathered) {
+				    base = base + gathered[t];
+				}
+				gathered[t]=base;
+			    }
+			}
+		    }
+		}
+		for (var k in gathered) {
+		    data.push({order: k,cost: gathered[k]});
+		};
+		
+		var chart = AmCharts.makeChart("cost-bd", {
+			"type": "pie",
+			"colors": colorArray,
+			
+			"dataProvider": data,
+			"titleField": "order",
+			"valueField": "cost",
+			"labelRadius": 5,
+			"pullOutOnlyOne": true,
+			
+			"radius": "25%",
+			"innerRadius": "60%",
+			"labelText": "[[title]]: $[[value]]"
+			//"balloonText": "Expenses in [[title]]:<b>$[[value]]</b>"
+		    });
+		// Note that this refers to the object whose change generated the call to update
+		// In this case that is a spendingModel object.
+		var that=this;
+		chart.addListener("pullOutSlice", function(x) {that.clickType(x,that);});
+	    }
+	});
     return CostBD;
-});
+       });
 
 //Chart codes are in the template file costbd.html
--- app/frontend_web/frontend/js/views/chart/spending.js
+++ app/frontend_web/frontend/js/views/chart/spending.js
@@ -9,18 +9,16 @@ define([
     'backbone',    // lib/backbone/backbone
     'currentContext',
     //model
-    'models/chartsModels/spendingModel',
     'text!templates/chart/spending.html'
-	], function ($, _, Backbone, currentContext, SpendingModel, spendingTemplate) {
+	], function ($, _, Backbone, currentContext, spendingTemplate) {
 
     var Spending = Backbone.View.extend({
         el: '.spending',
         template: _.template(spendingTemplate),
         initialize: function () {
             _.bindAll(this, 'render');
-	    this.spend = new SpendingModel;
             this.render();
-        },
+	    },
         render: function () {
             console.log('spending');
             var that = this;
@@ -31,101 +29,110 @@ define([
 	    
 	    that.$el.html(that.template({patientName: patientName,
 					 display:currentContext.get('display')}));
-
-            this.spend.fetch({
-                success: function (spend) {
-
-		    var colorArray = ["#0188BB", "#4C2694", "#79B32D", "#FF8500", "#00587A" ]
-			//Palette URL: http://colorschemedesigner.com/#3q62mWSE5w0w0
-			
-                    var category = new Array("Medication", "Labs", "Procedures", "RJS Nursing", "Immunization/Injection");
-		    
-		    var encs_cost = [];
-
-		    var keys = []
-			for (var d in spend.attributes) {
-			    keys.push(d);
-			}
-		    keys.sort();
-		    //console.log(spend);
-		    //console.log('keys');
-		    //console.log(keys);
-		    for (var k in keys) {
-			var d = keys[k];
-			var days_spend = spend.get(d);
-			//console.log('outer loop');
-			//console.log(days_spend);
-			var total=0;
-			for (var t in days_spend) {
-			    //console.log(days_spend[t]);
+	    
+	    },
+        events:{
+            'click': 'click'
+        },
+        click:function(){
+          console.log('clicked');
+        },
+	update: function(spend) {
+		titlefilter = null;
+		if('date' in spend.changed) {
+		    return;
+		}
+		if('title' in spend.changed) {
+		    titlefilter=spend.changed['title'];
+		}
+		
+		var colorArray = ["#0188BB", "#4C2694", "#79B32D", "#FF8500", "#00587A" ]
+		//Palette URL: http://colorschemedesigner.com/#3q62mWSE5w0w0
+		
+		var category = {};
+		
+		var encs_cost = [];
+		
+		var keys = []
+		for (var d in spend.attributes) {
+		    if(Date.parse(d)) {
+			keys.push(d);
+		    }
+		}
+		keys.sort();
+		for (var k in keys) {
+		    var d = keys[k];
+		    var days_spend = spend.get(d);
+		    var total=0;
+		    for (var t in days_spend) {
+			if(!titlefilter || t==titlefilter) {
 			    total += days_spend[t];
 			}
-			encs_cost.push({
-				date: new Date(d),
-				    encounter_cost: total
-				    });
-		    };
-		    
-		    //console.log('chart');
-		    //console.log(encs_cost);
-		    
-		    var chart = AmCharts.makeChart("total-spend", {
-			    "type": "serial",
-			    "dataDateFormat": "YYYY-MM-DD",
-			    "colors": colorArray,
-			    "pathToImages": "js/amcharts/images/",
-			    "dataProvider": encs_cost,
-			    "valueAxes": [
-		    {
-			"axisAlpha": 1,
-			"dashLength": 1,
-			"position": "left"
 		    }
-					  ],
-			    "graphs": [
-		    {
-			"id": "g1",
-			"balloonText": "[[category]]<br /><b><span style='font-size:14px;'>$[[value]]</span></b>",
-			//"bullet": "round",
-			//"bulletBorderAlpha": 1,
-			//"bulletColor":"#FFFFFF",
-			//"hideBulletsCount": 30,
-			// "title": "red line",
-			"valueField": 'encounter_cost',
-			//"useLineColorForBulletBorder":true,
-			"fillAlphas": .8,
-			"fillColorsField": "fill",
-			"inside": true,
-			"type": "column"
-		    }
-				       ],
-			    "chartScrollbar": {
-				"autoGridCount": true,
-				"graph": "g1",
-				"scrollbarHeight": 40
-			    },
-			    
-			    "chartCursor": {
-				"cursorPosition": "mouse",
-				"cursorColor": colorArray[3]
-			    },
-			    "categoryField": 'date',
-			    "categoryAxis": {
-				"parseDates": true,
-				"axisColor": "#DADADA",
-				"dashLength": 1,
-				"minorGridEnabled": true
-			    }
-			    
-			});
-		    
-
-                },
-                data: $.param(currentContext.toJSON())
-            });
-            return this;
-        }
+		    encs_cost.push({
+			    date: new Date(d),
+				encounter_cost: total
+				});
+		};
+		
+		var chart = AmCharts.makeChart("total-spend", {
+			"type": "serial",
+			"dataDateFormat": "YYYY-MM-DD",
+			"colors": colorArray,
+			"pathToImages": "js/amcharts/images/",
+			"dataProvider": encs_cost,
+			"valueAxes": [
+		{
+		    "axisAlpha": 1,
+		    "dashLength": 1,
+		    "position": "left"
+		}
+				      ],
+			"graphs": [
+		{
+		    "id": "g1",
+		    "balloonText": "[[category]]<br /><b><span style='font-size:14px;'>$[[value]]</span></b>",
+		    //"bullet": "round",
+		    //"bulletBorderAlpha": 1,
+		    //"bulletColor":"#FFFFFF",
+		    //"hideBulletsCount": 30,
+		    // "title": "red line",
+		    "valueField": 'encounter_cost',
+		    //"useLineColorForBulletBorder":true,
+		    "fillAlphas": .8,
+		    "fillColorsField": "fill",
+		    "inside": true,
+		    "type": "column"
+		}
+				   ],
+			"chartScrollbar": {
+			    "autoGridCount": true,
+			    "graph": "g1",
+			    "scrollbarHeight": 40
+			},
+			
+			"chartCursor": {
+			    "cursorPosition": "mouse",
+			    "cursorColor": colorArray[3]
+			},
+			"categoryField": 'date',
+			"categoryAxis": {
+			    "parseDates": true,
+			    "axisColor": "#DADADA",
+			    "dashLength": 1,
+			    "minorGridEnabled": true
+			}
+			
+		    });
+		// Note that this refers to the object whose change generated the call to update
+		// In this case that is a spendingModel object.
+		var that=this;
+		chart.addListener("clickGraphItem", function(x) {that.clickDate(x,that);});
+	    }
 	});
+       
+	   
+	   
     return Spending;
-});
+       });
 //Chart codes are in the template file spending.html
