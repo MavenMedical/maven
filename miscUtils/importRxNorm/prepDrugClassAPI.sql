e601732d250f38d82e17a06bf0ffa0519f828387
dave@mavenmedical.net <dave@localhost.localdomain>
2014-05-05 17:34:54 -0700

    MAV-147 	modified:   prepDrugClassAPI.sql

--- miscUtils/importRxNorm/prepDrugClassAPI.sql
+++ miscUtils/importRxNorm/prepDrugClassAPI.sql
@@ -12,13 +12,27 @@ create index ixDrugClasssnomed on terminology.drugClass (snomedid);
 
 
 insert into terminology.drugclass (
-	select distinct a.rxaui,a.rxcui,b.str from rxnsat a 
-	inner join rxnconso b on a.rxaui=b.rxaui
+	select distinct a.rxaui,a.rxcui,b.str from terminology.rxnsat a 
+	inner join terminology.rxnconso b on a.rxaui=b.rxaui
 	where (a.atn='ATC_LEVEL' and a.sab='ATC' and a.atv='4')
 );
 
 
-update drugclass dc set snomedid= (select min(cast(scui as numeric)) from rxnconso a where a.rxcui=dc.rxcui and a.sab ='SNOMEDCT_US' and tty='PT');
+update terminology.drugclass dc set snomedid= (select min(cast(scui as numeric)) from rxnconso a where a.rxcui=dc.rxcui and a.sab ='SNOMEDCT_US' and tty='PT');
+
+create table terminology.doseformGroups (rxcui varchar(20) primary key, groupname varchar(200));
+create index ixdoseformgrouppk on terminology.doseformgroups (rxcui);
+insert into doseformgroups 
+(
+	select rxcui,max(grp) from (
+	select distinct a.rxcui,case when lower(str) like '%oral%' or lower(str) like '%extend%tab%' or lower(str) like '%pill%' or lower(str) like '%chewable%' or lower(str) like ('%tab%extend%') 
+	or lower(str) like '%extend%cap%' or lower(str) like '%cap%extend%' then 'Oral' 
+	when lower(str) like '%inject%' or lower(str) like '%syringe%' then 'Injection'
+	when lower(str) like '%topical%' then 'Topical'
+	else lower(str) end grp from terminology.rxnconso a inner join terminology.rxnrel b on a.rxcui=b.rxcui2 where tty='DF')
+	a group by rxcui
+);
+
 
 create table terminology.drugClassAncestry
 (
@@ -27,28 +41,31 @@ create table terminology.drugClassAncestry
    ,brandname varchar(200)
    ,routename varchar(200)
    ,ndc varchar(200)
-   ,primary key (classaui,inclassaui)
+   ,classname varchar(200)
+   ,drug varchar(200)
+   ,primary key (classaui,inclassaui,ndc)
 );
-create index ixDrugClassAncestryPK on terminology.drugClassAncestry (classaui,inclassaui);
-create index ixDrugClassAncestryReverse on terminology.drugClassAncestry (inclassaui,classaui);
+create index ixDrugClassAncestryPK on terminology.drugClassAncestry (classaui,inclassaui,ndc);
+create index ixDrugClassAncestryReverse on terminology.drugClassAncestry (inclassaui,classaui,ndc);
+create index ixDrugClassAncestryNDC on terminology.drugclassancestry(ndc,classaui,inclassaui);
 
 
 insert into terminology.drugclassancestry(
-select distinct  dc.rxaui ,ndc.rxaui ,max(coalesce(brand.str,member.str||' (generic)')) ,max(route.groupname),max(ndc.atv)
+select distinct  dc.rxaui ,ndc.rxaui ,max(coalesce(brand.str,member.str||' (generic)')) ,max(route.groupname),ndc.atv,max(dc.str),max(member.str)
 from drugclass dc
-inner join rxnrel a on a.rxaui1=dc.rxaui and a.rela='member_of'
-inner join rxnconso member on member.rxaui=a.rxaui2 
-inner join rxnrel rel2 on rel2.rxcui1=member.rxcui and rel2.rela='has_ingredient'
-inner join rxnconso drug on drug.rxcui=rel2.rxcui2 and drug.sab='RXNORM' and drug.tty!='TMSY'
-inner join rxnrel rel3 on rel3.rxcui1=drug.rxcui and rel3.rela in ('consists_of','isa')
-inner join rxnconso soldas on soldas.rxcui=rel3.rxcui2 and soldas.sab='RXNORM' and soldas.tty='SY'
+inner join terminology.rxnrel a on a.rxaui1=dc.rxaui and a.rela='member_of'
+inner join terminology.rxnconso member on member.rxaui=a.rxaui2 
+inner join terminology.rxnrel rel2 on rel2.rxcui1=member.rxcui and rel2.rela='has_ingredient'
+inner join terminology.rxnconso drug on drug.rxcui=rel2.rxcui2 and drug.sab='RXNORM' and drug.tty!='TMSY'
+inner join terminology.rxnrel rel3 on rel3.rxcui1=drug.rxcui and rel3.rela in ('consists_of','isa')
+inner join terminology.rxnconso soldas on soldas.rxcui=rel3.rxcui2 and soldas.sab='RXNORM' and soldas.tty='SY'
 --check if there's an NDC for the thing
-inner join rxnsat ndc on ndc.rxcui=soldas.rxcui and ndc.atn='NDC'  and ndc.sab='RXNORM' 
-inner join rxnrel routerel on routerel.rxcui1=ndc.rxcui and routerel.rela='dose_form_of'
-inner join doseformgroups route on routerel.rxcui2=route.rxcui
-left outer join rxnrel brel on brel.rxcui1=ndc.rxcui and brel.rela='ingredient_of'
-left outer join rxnconso brand on brel.rxcui2=brand.rxcui and brand.tty='BN'
-group by dc.rxaui ,ndc.rxaui 
+inner join terminology.rxnsat ndc on ndc.rxcui=soldas.rxcui and ndc.atn='NDC'  and ndc.sab='RXNORM' 
+inner join terminology.rxnrel routerel on routerel.rxcui1=ndc.rxcui and routerel.rela='dose_form_of'
+inner join terminology.doseformgroups route on routerel.rxcui2=route.rxcui
+left outer join terminology.rxnrel brel on brel.rxcui1=ndc.rxcui and brel.rela='ingredient_of'
+left outer join terminology.rxnconso brand on brel.rxcui2=brand.rxcui and brand.tty='BN'
+group by dc.rxaui ,ndc.rxaui,ndc.atv
 );
 
 create or replace function getClassNameFromNDC(ndc varchar(20))
@@ -57,8 +74,8 @@ $BODY$
 Declare rtn varchar(200);
 begin
         select distinct c.str into rtn from rxnsat a
-	inner join drugClassAncestry b on a.rxaui=b.inclassaui
-	inner join drugclass c on b.classaui=c.rxaui
+	inner join terminology.drugClassAncestry b on a.rxaui=b.inclassaui
+	inner join terminology.drugclass c on b.classaui=c.rxaui
 	 where a.atn='NDC' and a.atv=ndc and a.sab='RXNORM' ;
 	 
         return rtn;
@@ -72,8 +89,8 @@ $BODY$
 Declare rtn numeric;
 begin
         select distinct c.SnomedId into rtn from rxnsat a
-	inner join drugClassAncestry b on a.rxaui=b.inclassaui
-	inner join drugclass c on b.classaui=c.rxaui
+	inner join terminology.drugClassAncestry b on a.rxaui=b.inclassaui
+	inner join terminology.drugclass c on b.classaui=c.rxaui
 	 where a.atn='NDC' and a.atv=ndc and a.sab='RXNORM' ;
 	 
         return rtn;
@@ -102,13 +119,45 @@ Declare rtn Boolean;
 Declare Sno numeric;
 begin
         select b.snomedid into sno
-		from drugclassancestry a 
-		inner join drugclass b on a.classaui=b.rxaui
+		from terminology.drugclassancestry a 
+		inner join terminology.drugclass b on a.classaui=b.rxaui
 		where inclassaui=vrxaui and snomedid is not null;
         return isSnomedChild(vparentconcept,sno);
 end;
 $BODY$
   LANGUAGE plpgsql ;
+
+create or replace function getNadacFromNDC(v_ndc varchar(20)) 
+returns numeric(18,2) as
+$BODY$
+declare nadacCost numeric(18,2);
+begin
+ select unitcost into nadacCost from public.nadac where ndc=v_ndc limit 1;
+ return nadaccost;
+end;
+$BODY$
+   language plpgsql;
+   
+create or replace function GetAlternativeTable(v_ndc varchar(20)) 
+returns table(brandname varchar(200),relCost numeric(18,2),aproxCost numeric(18,2)) as
+$BODY$
+declare nadacCost numeric(18,2);
+begin
+      select getNadacFromNDC(v_ndc) into nadacCost;
+      return query select distinct x.brandname,avg(nadac.unitcost),avg(nadac.unitcost)*nadacCost
+	 from terminology.drugClassAncestry b 
+		inner join terminology.drugClassAncestry x on x.classaui=b.classaui
+		inner join public.nadac nadac on nadac.ndc=x.ndc
+		 where b.ndc=v_ndc
+		 and x.routename=b.routename
+	group by x.brandname;
+
+end;
+$BODY$
+   language plpgsql;
+--select * from getalternativetable('00002323130')
+
+
 
 /****************Do not use********************
  CREATE OR REPLACE FUNCTION terminology.isRxCUIchild(vparentconcept numeric, vrxCui character varying)
