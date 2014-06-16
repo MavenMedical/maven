3aeb579fae43ef43c8409ef042c61da1c4ea26e8
dave@mavenmedical.net <dave@localhost.localdomain>
2014-06-16 15:42:03 -0700

    MAV-186 parse new DB format and new MS Word Shorthand.  Changes to be committed: 	modified:   docParser.cs

--- miscUtils/c#RuleParser/docParser.cs
+++ miscUtils/c#RuleParser/docParser.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +14,7 @@ namespace ChoosingWiselyDocParser
     class Program
     {
         public static NpgsqlConnection db=new NpgsqlConnection("Server=192.168.40.133;Port=5432;Database=maven;User Id=maven;Password=temporary;");
+        //public static NpgsqlConnection db = new NpgsqlConnection("Server=23.251.149.125;Port=5432;Database=maven;User Id=maven;Password=mavendevel;");
         static void Main(string[] args)
         {
             DirectoryInfo di=new DirectoryInfo(@"C:\Users\dave\MavenDrive\Choosing Wisely Review\technical encoding");
@@ -36,18 +37,19 @@ namespace ChoosingWiselyDocParser
                     doctext += para.Range.Text;
                 }
                 ArrayList remlines = new ArrayList();
-                string codetype="CPT";
+                string codetype="";
                 string[] trigs = GetTrigcodes(doctext, ref remlines,ref codetype);
-                string[] encdx = GetEncDxcodes(ref remlines);
-                string[] problist = GetPLDxcodes(ref remlines);
+                string[] encdx = GetDxCodeLists(doctext);
+                string[] prochist = GetPxcodes(doctext);
+                string[] labcrit = GetLabCriteria(doctext);
                 decimal minage = -1;
-                decimal maxage = -1;
+                decimal maxage = 200;
                 string rulename = fi.Name.Replace(".docx", "");
                 getAges(ref remlines, ref minage, ref maxage);
                 string sex=getSex(ref remlines);
                 string details = getDetails(remlines);
                 doc.Close();
-                InsertRule(rulename, minage, maxage, sex, codetype, details, trigs, encdx, problist);
+                InsertRule(rulename, minage, maxage, sex, codetype, details, trigs, encdx,prochist,labcrit);
             }
             
         }
@@ -69,6 +71,7 @@ namespace ChoosingWiselyDocParser
         public static string[] GetTrigcodes(string doctxt, ref ArrayList RemainingLines,ref string codetype)
         {
             codetype="CPT";
+            string routeclause="1=1";
             RemainingLines = new ArrayList();
             ArrayList al = new ArrayList();
             doctxt = doctxt.Replace("\r", "|");
@@ -80,14 +83,25 @@ namespace ChoosingWiselyDocParser
                 string cur = lines[i];
                 if (cur.Contains("ptrig")) inPtrigBlock=true;
                 if (cur.Contains("/ptrig")) inPtrigBlock=false;
-                if (inPtrigBlock && !cur.Contains("ptrig") && cur.Contains("{"))
+                if (inPtrigBlock && !cur.Contains("ptrig") && cur.Contains("{trigcode"))
                 {
                     string trigcode = cur.Split('{')[1].Replace("trigcode:", "").Replace("}", "").Trim();
                     al.Add(trigcode);
                 }
+                else if (inPtrigBlock && !cur.Contains("ptrig") && cur.Contains("{route"))
+                {
+                    routeclause="routename in (";
+                    string[] list=cur.Replace("{route:","").Replace("}","").Split(',');
+                    foreach (string route in list)
+                    {
+                        routeclause+="'"+route.Trim()+"',";
+                    }
+                    routeclause=routeclause.TrimEnd(',')+")";
+                    
+                }
                 else if (inPtrigBlock && cur.Contains("CODETYPE"))
                 {
-                    codetype=cur.Split(':')[1].Trim();
+                    codetype=cur.Split(':')[2].Trim();
                 }
                 else
                 {
@@ -95,12 +109,121 @@ namespace ChoosingWiselyDocParser
                 }
                 i++;
             }
-
+            if (codetype == "MED")
+            {
+                al = expandMedList(al,routeclause);
+            }
             string[] rtn=new string[al.Count];
             al.CopyTo(rtn);
             return rtn;
         }
-        public static string[] GetEncDxcodes(ref ArrayList RemainingLines)
+        public static ArrayList expandMedList(ArrayList snomeds,string routeclause)
+        {
+            string sql="select distinct ndc from terminology.drugclassancestry a inner join terminology.drugclass b on a.classaui=b.rxaui "
+                        +"inner join terminology.conceptancestry c on b.snomedid=c.child where c.ancestor=@snomed and "+routeclause;
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, db);
+            ArrayList rtn = new ArrayList();
+            db.Open();
+            foreach (string sno in snomeds)
+            {
+                cmd.Parameters.Add("@snomed", Convert.ToInt64(sno));
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ndc=reader.GetString(0);
+                    if(!rtn.Contains(ndc))
+                    {
+                        rtn.Add(ndc);
+                    }
+                }
+            }
+            db.Close();
+            return rtn;
+        }
+        /// <summary>
+        /// Expects a list of codes like this
+        ///     dxor:ENC:false
+        ///     snomed: 312117008
+        ///     snomed: 255320000
+        ///     /dxor
+        /// </summary>
+        /// <param name="doctext"></param>
+        /// <returns></returns>
+        public static string[] GetDxCodeLists(string doctext)
+        {
+            ArrayList al = new ArrayList();
+            string pattern = @"dxor.*?/dxor"; //.*? matches the shortest possible string wrapped in dxor.../dxor
+            MatchCollection matches= Regex.Matches(doctext, pattern,RegexOptions.Multiline);
+            foreach (Match mch in matches)
+            {
+                string txt = mch.ToString();
+                string type = "'"+txt.Split('\r')[0].Split(':')[1].Trim()+"'";
+                string isintersect = "'"+txt.Split('\r')[0].Split(':')[2].Trim()+"'";
+                string framemin = "-99999";
+                string framemax = "0";
+                string intlist="array[]";
+                try
+                {
+                    framemin = txt.Split('\r')[0].Split(':')[3].Trim();
+                    framemax = txt.Split('\r')[0].Split(':')[4].Trim();
+                }
+                catch{/*whatever, just use the default fram if not specified*/}
+                string a = "insert into rules.codelists (ruleid, listtype,isintersect,intlist,strlist,framemin,framemax) values(<<ruleid>>,"+type
+                    +","+isintersect+",<<intlist>>,null,"+framemin+","+framemax+")";
+                string[] splitter = { "snomed:"};
+                string[] snomedlist=txt.Replace("/dxor","").Split(splitter,StringSplitOptions.None);
+                bool isfirst = true;
+                foreach (string sno in snomedlist)
+                {
+                    if (!isfirst) { intlist = snomedToArraySyntax(sno.Trim(), intlist); }
+                    isfirst = false;
+                }
+                a = a.Replace("<<intlist>>", intlist);
+                al.Add(a);
+            }
+            string[] rtn = new string[al.Count];
+            al.CopyTo(rtn);
+            return rtn;
+        }
+        public static string snomedToArraySyntax(string snomed, string addto)
+        {
+            string rtn = addto.TrimEnd(']');
+            if (!rtn.EndsWith("[")) {rtn+=",";} //if we already have stuff in the array, append it with a comma to continue adding values
+            string sql = "select distinct child from terminology.conceptancestry where ancestor=@sno";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, db);
+            cmd.Parameters.Add("@sno", Convert.ToInt64(snomed));
+            db.Open();
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string child = Convert.ToString(reader.GetDecimal(0));
+                if (!rtn.Contains(","+child+","))
+                {
+                    rtn+=child+",";
+                }
+            }
+            db.Close();
+            rtn = rtn.TrimEnd(',') + "]";
+            return rtn;
+        }
+        public static string stringsToArraySyntax(string cpts, string addto)
+        {
+            string rtn = addto.TrimEnd(']');
+            if (!rtn.EndsWith("[")) { rtn += ","; } //if we already have stuff in the array, append it with a comma to continue adding values
+            string[] cptlist = cpts.Split(',');
+            foreach ( string cpt in cptlist)
+            {
+                string child = "'"+cpt.Trim()+"'";
+                if (!rtn.Contains("," + child + ","))
+                {
+                    rtn += child + ",";
+                }
+            }
+            db.Close();
+            rtn = rtn.TrimEnd(',') + "]";
+            return rtn;
+        }
+        public static string[] oldGetEncDxcodes(ref ArrayList RemainingLines)
         {
 
             ArrayList al = new ArrayList();
@@ -130,39 +253,106 @@ namespace ChoosingWiselyDocParser
             al.CopyTo(rtn);
             return rtn;
         }
-        public static string[] GetPLDxcodes(ref ArrayList RemainingLines)
+        /// <summary>
+        /// Expects the following to denote a historic procedure list
+        ///     pxor:HXPX:true
+        ///     cpt:1,2,3a,4
+        ///     cpt:5.1a,6
+        ///     /pxor
+        /// </summary>
+        /// <param name="doctext"></param>
+        /// <returns></returns>
+        public static string[] GetPxcodes(string doctext)
         {
 
             ArrayList al = new ArrayList();
-            string[] lines = new string[RemainingLines.Count];
-            RemainingLines.CopyTo(lines);
-            RemainingLines = new ArrayList();
-            int i = 0;
-            bool inPtrigBlock = false;
-            while (i < lines.Count())
+            string pattern = @"pxor.*?/pxor"; //.*? matches the shortest possible string wrapped in dxor.../dxor
+            MatchCollection matches = Regex.Matches(doctext, pattern, RegexOptions.Multiline);
+            foreach (Match mch in matches)
             {
-                string cur = lines[i];
-                if (cur.Contains("plor")) inPtrigBlock = true;
-                if (cur.Contains("/plor")) inPtrigBlock = false;
-                if (inPtrigBlock && !cur.Contains("plor")&&cur.Contains("{"))
+                string txt = mch.ToString();
+                string type = "'" + txt.Split('\r')[0].Split(':')[1].Trim() + "'";
+                string isintersect = "'" + txt.Split('\r')[0].Split(':')[2].Trim() + "'";
+                string framemin = "-99999";
+                string framemax = "0";
+                string intlist = "array[]";
+                try
                 {
-                    string trigcode = cur.Split('{')[1].Replace("\"", "").Replace("type:problem_list", "").Replace("exists:true", "").Replace("snomed:", "").Replace(",", "").Replace("}", "").Trim();
-                    al.Add(trigcode);
+                    framemin = txt.Split('\r')[0].Split(':')[3].Trim();
+                    framemax = txt.Split('\r')[0].Split(':')[4].Trim();
                 }
-                else
+                catch {/*whatever, just use the default fram if not specified*/}
+                string a = "insert into rules.codelists (ruleid, listtype,isintersect,intlist,strlist,framemin,framemax) values(<<ruleid>>," + type
+                    + "," + isintersect + ",null,<<strlist>>," + framemin + "," + framemax + ")";
+                string[] splitter = { "cpt:" };
+                string[] pxlist = txt.Replace("/pxor", "").Split(splitter, StringSplitOptions.None);
+                bool isfirst = true;
+                foreach (string px in pxlist)
                 {
-                    RemainingLines.Add(cur);
+                    if (!isfirst) { intlist = stringsToArraySyntax(px.Trim(), intlist); }
+                    isfirst = false;
                 }
-                i++;
+                a = a.Replace("<<strlist>>", intlist);
+                al.Add(a);
             }
+            string[] rtn = new string[al.Count];
+            al.CopyTo(rtn);
+            return rtn;
+        }
+        /// <summary>
+        /// Expects the following to denote a historic procedure list (case sensitive)
+        ///     labcrit:treshold:relation:defaultbool:framemin,framemax
+        ///     loincs:1,2,3a,4
+        ///     /labcrit
+        /// 
+        /// for example:
+        ///     labcrit:10000:>:true:-99999:0
+        ///     loincs:123,234,455
+        ///     /labcrit
+        /// </summary>
+        /// <param name="doctext"></param>
+        /// <returns></returns>
+        public static string[] GetLabCriteria(string doctext)
+        {
 
+            ArrayList al = new ArrayList();
+            string pattern = @"labcrit.*?/labcrit"; //.*? matches the shortest possible string wrapped in dxor.../dxor
+            MatchCollection matches = Regex.Matches(doctext, pattern, RegexOptions.Multiline);
+            foreach (Match mch in matches)
+            {
+                string txt = mch.ToString();
+                string thresh =  txt.Split('\r')[0].Split(':')[1].Trim() ;
+                string rel = "'" + txt.Split('\r')[0].Split(':')[2].Trim() + "'";
+                string dflt =  txt.Split('\r')[0].Split(':')[3].Trim() ;
+                string framemin = "-99999";
+                string framemax = "0";
+                string intlist = "array[]";
+                try
+                {
+                    framemin = txt.Split('\r')[0].Split(':')[4].Trim();
+                    framemax = txt.Split('\r')[0].Split(':')[5].Trim();
+                }
+                catch {/*whatever, just use the default fram if not specified*/}
+                string a = "insert into rules.labeval (ruleid, loinc_codes,threshold,relation,framemin,framemax,defaultval) values(<<ruleid>>,<<loincs>>," + thresh
+                    + "," + rel + "," + framemin + "," + framemax +","+dflt+ ")";
+                string[] splitter = { "loincs:" };
+                string[] pxlist = txt.Replace("/labcrit", "").Split(splitter, StringSplitOptions.None);
+                bool isfirst = true;
+                foreach (string px in pxlist)
+                {
+                    if (!isfirst) { intlist = stringsToArraySyntax(px.Trim(), intlist); }
+                    isfirst = false;
+                }
+                a = a.Replace("<<loincs>>", intlist);
+                al.Add(a);
+            }
             string[] rtn = new string[al.Count];
             al.CopyTo(rtn);
             return rtn;
         }
         public static string getSex(ref ArrayList RemainngLines)
         {
-            string rtn="*";
+            string rtn="%";
             ArrayList newRemLine = new ArrayList();
             foreach (string line in RemainngLines)
             {
@@ -216,11 +406,11 @@ namespace ChoosingWiselyDocParser
             db.Close();
             return rtn;
         }
-        public static void InsertRule(string name, decimal minage, decimal maxage, string sex, string codetype, string details, string[] trigcodes, string[] encdx, string[] plist)
+        public static void InsertRule(string name, decimal minage, decimal maxage, string sex, string codetype, string details, string[] trigcodes, string[] encdx,string[] prochist, string[] labcrit)
         {
             int ruleid = getRuleId(name);
             deleteRule(ruleid);
-            string cmd = "insert into evirule (ruleid,name,minage,maxage,sex,codetype,details) values(@id,@name,@minage,@maxage,@sex,@codetype,@details)";
+            string cmd = "insert into rules.evirule (ruleid,name,minage,maxage,sex,codetype,remainingdetails) values(@id,@name,@minage,@maxage,@sex,@codetype,@details)";
             NpgsqlCommand sql = new NpgsqlCommand(cmd, db);
             sql.Parameters.Add("@id", ruleid);
             sql.Parameters.Add("@name", name);
@@ -231,22 +421,25 @@ namespace ChoosingWiselyDocParser
             sql.Parameters.Add("@details", details);
             db.Open();
             sql.ExecuteNonQuery();
-            sql.CommandText = "insert into ruletrigcodes values(@id,@trigcode)";
+            sql.CommandText = "insert into rules.trigcodes values(@id,@trigcode)";
             foreach(string code in trigcodes)
             {
                 sql.Parameters.Add("@trigcode", code);
                 sql.ExecuteNonQuery();
             }
-            sql.CommandText = "insert into ruleencdx values(@id,@snomed)";
-            foreach (string code in encdx)
+            foreach (string insrt in encdx)
             {
-                sql.Parameters.Add("@snomed", code);
+                sql.CommandText = insrt.Replace("<<ruleid>>",ruleid.ToString());
                 sql.ExecuteNonQuery();
             }
-            sql.CommandText = "insert into ruleproblist values(@id,@pl)";
-            foreach (string code in plist)
+            foreach (string insrt in prochist)
             {
-                sql.Parameters.Add("@pl", code);
+                sql.CommandText = insrt.Replace("<<ruleid>>", ruleid.ToString());
+                sql.ExecuteNonQuery();
+            }
+            foreach (string insrt in labcrit)
+            {
+                sql.CommandText = insrt.Replace("<<ruleid>>", ruleid.ToString());
                 sql.ExecuteNonQuery();
             }
             db.Close();
@@ -254,18 +447,17 @@ namespace ChoosingWiselyDocParser
         }
         public static void deleteRule(int ruleid)
         {
-            NpgsqlCommand sql = new NpgsqlCommand("delete from evirule where ruleId=@id", db);
+            NpgsqlCommand sql = new NpgsqlCommand("delete from rules.evirule where ruleId=@id", db);
             sql.Parameters.Add("@id", ruleid);
             db.Open();
             sql.ExecuteNonQuery();
-            sql.CommandText = "delete from ruletrigcodes where ruleId=@id";
+            sql.CommandText = "delete from rules.trigcodes where ruleId=@id";
             sql.ExecuteNonQuery();
-            sql.CommandText = "delete from ruleencdx where ruleId=@id";
+            sql.CommandText = "delete from rules.codelists where ruleId=@id";
             sql.ExecuteNonQuery();
-            sql.CommandText = "delete from ruleproblist where ruleId=@id";
+            sql.CommandText = "delete from rules.labeval where ruleId=@id";
             sql.ExecuteNonQuery();
             db.Close();
         }
     }
 }
-
