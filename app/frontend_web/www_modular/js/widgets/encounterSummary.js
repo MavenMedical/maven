7144666abff0ac1406aeb2dc145db04f6aa6ecad
aaljuhani <asmaaaljuhani@gmail.com>
2014-06-26 23:38:49 -0400

    MAV-211 datepicker with event

--- app/frontend_web/www_modular/js/widgets/encounterSummary.js
+++ app/frontend_web/www_modular/js/widgets/encounterSummary.js
@@ -11,7 +11,8 @@ define([
     'globalmodels/summaryModel', // current patient (if any)
     'globalmodels/patientModel',
     'globalmodels/contextModel',
-], function ($, _, Backbone, datepicker, summaryModel, patientModel, contextModel) {
+    'globalmodels/histogramModel'
+], function ($, _, Backbone, datepicker, summaryModel, patientModel, contextModel, histogramModel) {
 
     var Summary = Backbone.View.extend({
         initialize: function (arg) {
@@ -20,17 +21,36 @@ define([
             summaryModel.on('change', this.update, this);
             contextModel.on('change:patients change:encounter', this.update, this);
             patientModel.on('change:name', this.update, this);
+
             // Source for Date picker http://bootstrap-datepicker.readthedocs.org/en/release/index.html
             $('#datepicker').datepicker({
                 format: "mm/dd/yyyy",
                 autoclose: true,
+                endDate: new Date(), //calender end date is today
+                beforeShowDay: function (date) {
+                    for (var key in histogramModel.attributes) {
+                        var enc = histogramModel.attributes[key];
+                        if(date.setHours(0, 0, 0, 0) == new Date(enc['admission']).setHours(0, 0, 0, 0)){
+                           return {
+                                tooltip: 'Encounter Cost $'+enc['spending'],
+                                classes: 'admission'
+                           }
+                            //TODO: add the css class for all days between the admission and discharge
+                        }else{
+                            return false;
+                        }
+                    }
+                }
             })
                 .on('changeDate', function (e) {
                     console.log('date change');
+                    console.log(e);
                     console.log(e.date.toLocaleDateString()); //get picked Date
+                    contextModel.set({enc_date:e.date.toLocaleDateString()});
+                    console.log(contextModel.attributes);
                 });
 
-           // $('#datepicker').datepicker('update', new Date()); // set the encounter date here
+            $('#datepicker').datepicker('update', new Date('2014-03-24')); // set the encounter date here
         },
         update: function (summary) {
             if (summaryModel.get('spending')) {
@@ -40,8 +60,6 @@ define([
                 } else if (patientModel.get('name')) {
                     title = patientModel.get('name');
                 }
-                //console.log(title+": "+contextModel.get('encounter')+"  "+contextModel.get('patients'));
-                //console.log(contextModel);
                 this.$el.html(this.template($.extend({}, summaryModel.attributes, {'title': title})));
                 this.$el.show();
             } else {
@@ -50,4 +68,5 @@ define([
         }
     });
     return Summary;
-});
+})
+;
