06c491a751904de667a410c7a1c182cd557d2fbf
Yuki Uchino <yuki@mavenmedical.net>
2014-07-14 09:57:24 -0400

    Alter loadData orderable csv files to remove base_cost and add HCPCS coding

--- app/schema/data/loadData.sql
+++ app/schema/data/loadData.sql
@@ -4,5 +4,5 @@
 delete from public.costmap where customer_id=-1 and code_type='HCPCS';
 delete from public.orderable where customer_id=-1 and ord_type='Proc';
 \copy public.costmap from 'CostMapDefaults.csv' DELIMITER ','  null as '' CSV 
-\copy public.orderable(customer_id,orderable_id,ord_type,system,name,description,status,source,base_cost,cpt_code) from 'OrderableDefaults.csv' DELIMITER ',' null as '' quote as '"' CSV
+\copy public.orderable(customer_id,orderable_id,ord_type,system,name,description,status,source,cpt_code,cpt_version) from 'OrderableDefaults.csv' DELIMITER ',' null as '' quote as '"' CSV
 
