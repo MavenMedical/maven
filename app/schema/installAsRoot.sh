7e22c35c083dc95b9cd13f50dc2d9bc920b4374c
Yuki Uchino <yuki@mavenmedical.net>
2014-07-14 10:21:52 -0400

    Updates to database load scripts - drop public, rules, categories schemas before load

--- app/schema/installAsRoot.sh
+++ app/schema/installAsRoot.sh
@@ -1,4 +1,5 @@
-cat createDb.sql | sudo sudo -u postgres psql
+cat dropDb.sql | sudo sudo -u postgres psql -d maven
+cat createDb.sql | sudo sudo -u postgres psql -d maven
 cat createRulesDb.sql | sudo sudo -u postgres psql -d maven
 cd data
 ./loadData.sh
