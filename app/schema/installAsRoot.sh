906f240b4a330fabd4164884c01f47f0c4dc2da7
dave <dave@localhost.localdomain>
2014-07-16 12:32:33 -0700

    There was an issue with "cat file|sudo sudo psql -d maven" because the database maven doesnt exist on new installs. So this is remediating that. 	modified:   dropDb.sql 	modified:   installAsRoot.sh

--- app/schema/installAsRoot.sh
+++ app/schema/installAsRoot.sh
@@ -1,5 +1,5 @@
-cat dropDb.sql | sudo sudo -u postgres psql -d maven
-cat createDb.sql | sudo sudo -u postgres psql -d maven
+su postgres -c "psql -f dropDb.sql" 
+su postgres -c "psql -f createDb.sql"
 cat createRulesDb.sql | sudo sudo -u postgres psql -d maven
 cd data
 ./loadData.sh
