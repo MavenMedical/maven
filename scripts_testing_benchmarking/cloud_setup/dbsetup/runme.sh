c2eabfd8bf88219669755d50f12def682073d76a
root <root@CentOS2.(none)>
2014-07-25 12:13:09 -0500

    modified:   runme.sh

--- scripts_testing_benchmarking/cloud_setup/dbsetup/runme.sh
+++ scripts_testing_benchmarking/cloud_setup/dbsetup/runme.sh
@@ -1,6 +1,6 @@
 echo "This server will be setup as the initial master database server."
-read -p "Enter the local IP address for THIS server: " MST
-read -p "Enter the local IP address of the standby database server: " SBY
+read -p "Enter the local IP address for THIS server to setup a master database (or just enter for standby only): " MST
+read -p "Enter the local IP address of the standby database server (or just enter for local only): " SBY
 
 #SBY=192.168.1.9
 #MST=192.168.1.19
@@ -16,12 +16,25 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #ensure pg is stopped
 service postgresql-9.3 stop
-ssh $SBY service postgresql-9.3 stop
-
+if [ -n "$SBY" ]; then
+  ssh $SBY service postgresql-9.3 stop
+fi
 
 #setup the master configuration
-./master_setup.sh $SBY
-./copy_to_standby.sh $SBY
-ssh $SBY $DIR/sby_setup.sh $MST
-ssh $SBY $DIR/sby_complete.sh
+if [ -n "$MST" ]; then
+  ./master_setup.sh $SBY
+fi 
+
+#prep the standby server
+if [ -n "$SBY" ]; then
+  if [ -n "$MST" ]; then 
+    MST=ifconfig |grep "addr:"|head -n 1|cut -d ":" -f2|cut -d " " -f1
+  fi
+  echo "About to create a standby database on $SBY."
+  echo "This command should ONLY be run from the master node."
+  read -p "Hit enter to continue. (Only continue from the master node)"
+  ./copy_to_standby.sh $SBY
+  ssh $SBY $DIR/sby_setup.sh $MST
+  ssh $SBY $DIR/sby_complete.sh
+fi
 
