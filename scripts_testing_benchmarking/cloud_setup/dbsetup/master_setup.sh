b48cf903627e41ddf7be9f9b8439f9ee8269aa9c
root <root@CentOS2.(none)>
2014-07-25 09:51:25 -0500

    new file:   copy_to_standby.sh 	modified:   master_setup.sh 	modified:   runme.sh

--- scripts_testing_benchmarking/cloud_setup/dbsetup/master_setup.sh
+++ scripts_testing_benchmarking/cloud_setup/dbsetup/master_setup.sh
@@ -1,4 +1,3 @@
-#TAKES the standby IP as a parameter
 
 perl -pi -e 's/#wal_level = minimal/wal_level=hot_standby/' /var/lib/pgsql/9.3/data/postgresql.conf
 perl -pi -e 's/#max_wal_senders = 0/max_wal_senders = 5/' /var/lib/pgsql/9.3/data/postgresql.conf
@@ -10,26 +9,10 @@ perl -pi -e 's/#wal_keep_segments = 0/wal_keep_segments = 32/' /var/lib/pgsql/9.
 
 service postgresql-9.3 start
 #install maven
-cd ../../../app/schema/
+cd ../../../app/schema
 ./installAsRoot.sh
 ./terminology/installAsRoot.sh
 ./terminology/rxnorm/installAsRoot.sh
 ./terminology/loinc/installAsRoot.sh
 
-service postgresql-9.3 stop
-scp -r /var/lib/pgsql/9.3/data/base/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/global/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_clog/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_log/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_multixact/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_notify/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_serial/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_snapshots/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_stat/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_stat_tmp/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_subtrans/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_tblspc/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_twophase/ $1:/var/lib/pgsql/9.3/data/
-scp -r /var/lib/pgsql/9.3/data/pg_xlog/ $1:/var/lib/pgsql/9.3/data/
-service postgresql-9.3 start
 
--- scripts_testing_benchmarking/cloud_setup/dbsetup/runme.sh
+++ scripts_testing_benchmarking/cloud_setup/dbsetup/runme.sh
@@ -1,9 +1,9 @@
 echo "This server will be setup as the initial master database server."
-#read -p "Enter the local IP address for THIS server: " MST
-#read -p "Enter the local IP address of the standby database server: " SBY
+read -p "Enter the local IP address for THIS server: " MST
+read -p "Enter the local IP address of the standby database server: " SBY
 
-SBY=192.168.1.9
-MST=192.168.1.19
+#SBY=192.168.1.9
+#MST=192.168.1.19
 
 read -p "User Equivalence needs to be set up for the root user before proceeding. Hit enter to continue. ctrl+c to quit."
 
@@ -21,6 +21,7 @@ ssh $SBY service postgresql-9.3 stop
 
 #setup the master configuration
 ./master_setup.sh $SBY
+./copy_to_standby.sh $SBY
 ssh $SBY $DIR/sby_setup.sh $MST
 ssh $SBY $DIR/sby_complete.sh
 
