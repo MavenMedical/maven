74b0bb0b106e43d990c3095a1c993f91345a0371
Carlos Brenneisen <ca.brenneisen@gmail.com>
2014-08-12 13:26:11 -0400

    Merge branch 'DEV' of https://github.com/MavenMedical/maven into DEV
    Added script to insert database records into layout table; needed to support new layout functionality.
    
    MAV-294

---
+++ app/schema/updates/data/20140812/1.sql
@@ -0,0 +1,12 @@
+INSERT INTO layouts VALUES (1, 1, 'patientInfo', 'patientInfo.html', 'fixed-topB-1-1', 1);
+INSERT INTO layouts VALUES (2, 1, 'patientSearch', 'patientSearch.html', 'fixed-topB-1-1', 1);
+INSERT INTO layouts VALUES (3, 1, 'topBanner', 'topBanner.html', 'fixed-topA-1-1', 1);
+INSERT INTO layouts VALUES (4, 1, 'patientList', 'patientList.html', 'rowA-1-1', 1);
+INSERT INTO layouts VALUES (5, 1, 'encounterSummary', 'encounterSummary.html', 'rowB-1-1', 1);
+INSERT INTO layouts VALUES (6, 1, 'orderList', 'orderScroll.html', 'rowC-1-1', 1);
+INSERT INTO layouts VALUES (7, 1, 'costtable', 'costbreakdown-table.html', 'rowD-1-1', 1);
+INSERT INTO layouts VALUES (8, 1, 'spend_histogram', 'spend_histogram.html', 'rowE-1-1', 1);
+INSERT INTO layouts VALUES (10, 1, 'datepicker-calendar', 'datepicker-calendar.html', 'datepicker-modal', 1);
+INSERT INTO layouts VALUES (11, 1, 'settings', 'settings.html', 'settings-modal', 1);
+INSERT INTO layouts VALUES (12, 1, 'providerProfile', 'providerProfile.html', 'profile-modal', 1);
+INSERT INTO layouts VALUES (9, 1, 'alertList', 'alertScroll.html', 'floating-right', 1);
