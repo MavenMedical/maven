0308c58309c01dcce2ecac42a997d9fdf8215015
Tom DuBois <tom@mavenmedical.net>
2014-08-13 17:39:19 -0400

    MAV-288 scheduler now connects to the notification server

--- clientApp/allscripts/allscripts_scheduler.py
+++ clientApp/allscripts/allscripts_scheduler.py
@@ -5,6 +5,8 @@ from enum import Enum
 from datetime import date, datetime, timedelta
 import re
 from dateutil.parser import parse
+import clientApp.module_webservice.notification_service as NS
+import maven_logging as ML
 
 icd9_match = re.compile('\(V?[0-9]+(?:\.[0-9]+)?\)')
 
@@ -19,12 +21,13 @@ class Types(Enum):
 
 class scheduler():
 
-    def __init__(self, configname):
+    def __init__(self, configname, messenger):
         self.config = MavenConfig[configname]
         self.apiname = self.config[CONFIG_API]
         self.allscripts_api = AHC.allscripts_api(self.apiname)
         self.processed = set()
         self.lastday = None
+        self.messenger = messenger
 
     @asyncio.coroutine
     def get_updated_schedule(self):
@@ -39,16 +42,19 @@ class scheduler():
                 try:
                     sched = yield from self.allscripts_api.GetSchedule('CliffHux', today)
                 except AHC.AllscriptsError as e:
-                    print(e)
+                    ML.EXCEPTION(e)
                 # print([(sch['patientID'], sch['ApptTime2'], sch['ProviderID']) for sch in sched])
+                tasks = set()
                 for appointment in sched:
                     patient = appointment['patientID']
                     provider = appointment['ProviderID']
                     if (patient, provider, today) not in self.processed:
-                        asyncio.Task(self.evaluate(patient, provider, today, first))
+                        tasks.add((patient, provider, today, first))
+                for task in tasks:
+                    asyncio.Task(self.evaluate(*task))
             except Exception as e:
-                print(e)
-            yield from asyncio.sleep(5)
+                ML.EXCEPTION(e)
+            yield from asyncio.sleep(30)
             first = False
 
     @asyncio.coroutine
@@ -56,7 +62,7 @@ class scheduler():
         try:
             # print('evaluating %s for %s' % (patient, provider))
             now = datetime.now()
-            prior = now - timedelta(seconds=1200)
+            prior = now - timedelta(seconds=12000)
         except:
             import traceback
             traceback.print_exc()
@@ -73,18 +79,16 @@ class scheduler():
                     newdoc = doctime and parse(doctime) >= prior
 
                     if match and newdoc:
-                        self.processed.add((patient, provider, today))
+                        # self.processed.add((patient, provider, today))
                         if not first:
                             start, stop = match.span()
-                            print((start, stop))
-                            print('firing ' + str((patient, provider, keywords[start:stop])))
+                            self.messenger(patient, provider, keywords[start:stop])
                             break
                 # processed.update({doc['DocumentID'] for doc in documents})
         except AHC.AllscriptsError as e:
-            print(e)
+            ML.EXCEPTION(e)
         except:
-            import traceback
-            traceback.print_exc()
+            ML.EXCEPTION(e)
 
 if __name__ == '__main__':
     MavenConfig['allscripts_old_demo'] = {
@@ -107,8 +111,23 @@ if __name__ == '__main__':
         AHC.CONFIG_APPPASSWORD: 'MavenPathways123!!',
     }
 
+    MavenConfig['notificationserver'] = {
+        NS.SP.CONFIG_HOST: 'localhost',
+        NS.SP.CONFIG_PORT: 8092,
+        NS.SP.CONFIG_PARSERTIMEOUT: 120,
+        NS.CONFIG_QUEUEDELAY: 30,
+    }
+
     MavenConfig['scheduler'] = {CONFIG_API: 'allscripts_demo'}
 
-    sched = scheduler('scheduler')
+    ns = NS.NotificationService('notificationserver')
+
+    def translate(patient, provider, icd9):
+        ML.DEBUG(provider)
+        ns.send_messages(provider,
+                         ["patient %s is set with icd9 %s" % (patient, icd9)])
+    sched = scheduler('scheduler', translate)
+
     loop = asyncio.get_event_loop()
+    ns.schedule(loop)
     print(loop.run_until_complete(sched.get_updated_schedule()))
