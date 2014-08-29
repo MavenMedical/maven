a0685231fd0c2bee5eaafd33116d354a726bfd1b
Tom DuBois <tom@mavenmedical.net>
2014-08-29 14:52:11 -0400

    updated run_maven to user the new web services

--- run_maven.sh
+++ run_maven.sh
@@ -8,5 +8,5 @@ ${MAVEN_ROOT}/env/bin/python ${MAVEN_ROOT}/clientApp/webservice/allscripts_serve
 sleep 1
 ${MAVEN_ROOT}/env/bin/python ${MAVEN_ROOT}/app/backend/evaluators/composition_evaluator.py &
 sleep 1
-${MAVEN_ROOT}/env/bin/python ${MAVEN_ROOT}/app/frontend_web/rest_interface/web_services.py &
+${MAVEN_ROOT}/env/bin/python ${MAVEN_ROOT}/app/backend/webservices/all_services.py &
 
