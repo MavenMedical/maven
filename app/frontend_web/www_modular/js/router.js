e0f277dfaedb4b70693e30bfaacbf0c6088e6300
Evan Fischell <edfischell@gmail.com>
2014-10-10 11:44:31 -0400

    stash

--- app/frontend_web/www_modular/js/router.js
+++ app/frontend_web/www_modular/js/router.js
@@ -37,7 +37,7 @@ define([
         "patient": [0, 9, 3],
         "episode": [0, 9, 3],
         "pathway": [0, 12, 0],
-        "pathEditor": [0, 12, 0],
+        "pathEditor": [0, 12, 0]
     };
     changePageLayout = function (page) {
 
