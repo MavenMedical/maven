e3a65f0e8b8d147706a778f49099408f256bdc8c
aaljuhani <asmaaaljuhani@gmail.com>
2014-11-10 13:53:50 -0500

    MAV-571 wrap long titles for side panel

--- app/frontend_web/www_modular/js/widgets/maveninfo.js
+++ app/frontend_web/www_modular/js/widgets/maveninfo.js
@@ -20,8 +20,8 @@ define([
                 this.dynamicLayout = false;
 
             },
-            test: function(){
-              console.log("hi there test");
+            test: function () {
+                console.log("hi there test");
             },
             showhide: function () {
                 this.$el.show();
@@ -32,55 +32,60 @@ define([
 
             },
             render: function () {
-                if (!treeContext.get('selectedNode')){
-		    this.hidePopup()
+                if (!treeContext.get('selectedNode')) {
+                    this.hidePopup()
                     return
                 }
-		if(treeContext.get('selectedNode') == this.selectedNode) {return}
+                if (treeContext.get('selectedNode') == this.selectedNode) {
+                    return
+                }
 
                 //console.log('selectedNode', treeContext.get('selectedNode'));
                 // for accounts with no pathways
-                if(typeof treeContext.get('selectedNode') !== 'undefined') {
+                if (typeof treeContext.get('selectedNode') !== 'undefined') {
 
                     var that = this;
                     // Don't show if there is no text or if selected node is Protocol
-                    if (treeContext.get('selectedNode').attributes.sidePanelText && !treeContext.get('selectedNode').attributes.isProtocol ) {
-                       this.hidePopup();
-                       // this.$el.hide(); // hide previous side-popup
+                    if (treeContext.get('selectedNode').attributes.sidePanelText && !treeContext.get('selectedNode').attributes.isProtocol) {
+                        this.hidePopup();
+                        // this.$el.hide(); // hide previous side-popup
                         this.selectedNode = treeContext.get('selectedNode');
                         this.$el.html(this.template(treeContext.get('selectedNode').attributes));
 
                         this.showPopup();
-console.log($("#mavenInfo-header").width(), $("#mavenInfo-title").outerWidth())
-                    } else {this.hidePopup();}
-                } else {this.hidePopup()}
+                        //console.log($("#mavenInfo-header").width(), $("#mavenInfo-title").outerWidth())
+                    } else {
+                        this.hidePopup();
+                    }
+                } else {
+                    this.hidePopup()
+                }
                 //         return this;
             },
             hidePopup: function () {
                 this.$el.hide();
                 this.selectedNode = treeContext.get('selectedNode');
             },
-            showPopup: function(){
-                 if (this.dynamicLayout) {
-                            $('#side-popup').css('top', treeContext.get('selectedNodeOffset').top);
-                            $('#side-popup').css('left', treeContext.get('selectedNodeOffset').left + treeContext.get('selectedNodeWidth') + 10);
-                      $('#side-popup').css('bottom', 'none');
-                            $('#side-popup').css('right', 'none');
-                        }else{
-                            $('#side-popup').css('top', 'none');
-                            $('#side-popup').css('left', 'none');
-                            $('#side-popup').css('bottom', 5);
-                            $('#side-popup').css('right', 5);
-                        }
+            showPopup: function () {
+                if (this.dynamicLayout) {
+                    $('#side-popup').css('top', treeContext.get('selectedNodeOffset').top);
+                    $('#side-popup').css('left', treeContext.get('selectedNodeOffset').left + treeContext.get('selectedNodeWidth') + 10);
+                    $('#side-popup').css('bottom', 'none');
+                    $('#side-popup').css('right', 'none');
+                } else {
+                    $('#side-popup').css('top', 'none');
+                    $('#side-popup').css('left', 'none');
+                    $('#side-popup').css('bottom', 5);
+                    $('#side-popup').css('right', 5);
+                }
 
-                        this.$el.show(1000);
+                this.$el.show(1000);
 
             },
-            toggleLayout: function(){
-                console.log('toggle', 'test toggle');
-                if (this.dynamicLayout){
+            toggleLayout: function () {
+                if (this.dynamicLayout) {
                     this.dynamicLayout = false;
-                }else{
+                } else {
                     this.dynamicLayout = true;
                 }
                 this.showPopup();
