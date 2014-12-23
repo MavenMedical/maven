e1cdb6cf79c05a8caa85bc07f4e4e17e60d3fd4d
Tom DuBois <tom@mavenmedical.net>
2014-12-23 12:54:29 -0500

    MAV-678 Took away bulleted/numbered lists from ckeditor because they don't display properly

--- app/frontend_web/www_modular/js/ckeditor-customize.js
+++ app/frontend_web/www_modular/js/ckeditor-customize.js
@@ -1,5 +1,5 @@
 define(['ckeditor2'], function() {
     console.log(CKEDITOR)
-    CKEDITOR.config.removeButtons = 'Flash,PageBreak,Iframe,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Save,NewPage,Templates,Print,Preview'
+    CKEDITOR.config.removeButtons = 'Flash,PageBreak,Iframe,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Save,NewPage,Templates,Print,Preview,NumberedList,BulletedList'
     return CKEDITOR
 })
