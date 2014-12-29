c4eb82e23cef76d029c1ec6b9d72b2cc1ee6d479
tom <tom@mavenmedical.net>
2014-12-29 14:58:58 +0000

    a file, used only when creating minified files, which imports html templates

---
+++ app/frontend_web/www_modular/js/textstub.js
@@ -0,0 +1,6 @@
+require([
+    'text!../templates/settings.html',
+    'text!../templates/auditScroll.html',
+    'text!../templates/customerList.html',
+    'text!../templates/sidebar.html'
+], function() {})
